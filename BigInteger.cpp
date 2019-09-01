78d7ac9e3b78bb9197cc5c821162916b1b81b266
Ice2Faith <45895088+Ice2Faith@users.noreply.github.com>
2019-09-01 15:41:28 +0800

    Add files via upload

--- BigInteger.cpp
+++ BigInteger.cpp
@@ -24,7 +24,7 @@ BigInteger::BigInteger(BigInteger & num)
 {
 	this->m_num = num.m_num;
 }
-BigInteger& BigInteger::operator=(int num)
+BigInteger& BigInteger::operator=(unsigned int num)
 {
 	BigInteger temp((unsigned int)num);
 	this->m_num = temp.m_num;
@@ -117,6 +117,14 @@ ostream & operator<<(ostream &out, BigInteger & num)
 	out << temp;
 	return out;
 }
+BigInteger BigInteger::operator + (unsigned int  num)
+{
+	return *this + BigInteger(num);
+}
+BigInteger BigInteger::operator*(unsigned int  num)
+{
+	return *this*BigInteger(num);
+}
 BigInteger BigInteger::operator*(BigInteger & num)
 {
 	BigInteger result=0;
@@ -148,6 +156,18 @@ BigInteger BigInteger::operator*(BigInteger & num)
 	}
 	return result;
 }
+
+BigInteger& BigInteger::operator++()
+{
+	*this = (*this) + BigInteger(1);
+	return *this;
+}
+BigInteger BigInteger::operator++(int)
+{
+	BigInteger ret = *this;
+	*this = *this + BigInteger(1);
+	return ret;
+}
 string BigInteger::toString()
 {
 	string temp;
@@ -161,4 +181,319 @@ string BigInteger::toString()
 		temp.push_back(*i);
 	}
 	return temp;
+}
+BigInteger BigInteger::pow(BigInteger & n)
+{
+	BigInteger bresult = 1;
+	for (BigInteger i = 0; i < n; i++)
+	{
+		bresult *= (*this);
+	}
+	return bresult;
+}
+BigInteger BigInteger::pow(unsigned int  n)
+{
+	return pow(BigInteger(n));
+}
+BigInteger BigInteger::factorial()
+{
+	BigInteger bresult = 1;
+	for (BigInteger i = 1; i <=*this; i ++)
+	{
+		bresult *= i;
+	}
+	return bresult;
+}
+bool BigInteger::operator>(BigInteger & num)
+{
+	string pthis = this->toString();
+	string pnum = num.toString();
+	if (pthis.length() > pnum.length())
+		return true;
+	else if (pthis.length() < pnum.length())
+		return false;
+	else
+	{
+		for (int i = 0; i < pthis.length(); i++)
+		{
+			if (pthis[i]>pnum[i])
+				return true;
+			else if (pthis[i] < pnum[i])
+				return false;
+		}
+		return false;
+	}
+}
+bool BigInteger::operator<(BigInteger & num)
+{
+	string pthis = this->toString();
+	string pnum = num.toString();
+	if (pthis.length() < pnum.length())
+		return true;
+	else if (pthis.length() > pnum.length())
+		return false;
+	else
+	{
+		for (int i = 0; i < pthis.length(); i++)
+		{
+			if (pthis[i]<pnum[i])
+				return true;
+			else if (pthis[i] > pnum[i])
+				return false;
+		}
+		return false;
+	}
+
+}
+bool BigInteger::operator>=(BigInteger & num)
+{
+	string pthis = this->toString();
+	string pnum = num.toString();
+	if (pthis.length() > pnum.length())
+		return true;
+	else if (pthis.length() < pnum.length())
+		return false;
+	else
+	{
+		for (int i = 0; i < pthis.length(); i++)
+		{
+			if (pthis[i]>pnum[i])
+				return true;
+			else if (pthis[i] < pnum[i])
+				return false;
+		}
+		return true;
+
+	}
+	
+}
+bool BigInteger::operator<=(BigInteger & num)
+{
+	string pthis = this->toString();
+	string pnum = num.toString();
+	if (pthis.length() < pnum.length())
+		return true;
+	else if (pthis.length() > pnum.length())
+		return false;
+	else
+	{
+		for (int i = 0; i < pthis.length(); i++)
+		{
+			if (pthis[i]<pnum[i])
+				return true;
+			else if (pthis[i] > pnum[i])
+				return false;
+		}
+		return true;
+
+	}
+}
+bool BigInteger::operator==(unsigned int num)
+{
+	return *this == BigInteger(num);
+}
+bool BigInteger::operator!=(unsigned int num)
+{
+	return *this != BigInteger(num);
+}
+bool BigInteger::operator>(unsigned int num)
+{
+	return *this > BigInteger(num);
+}
+bool BigInteger::operator<(unsigned int num)
+{
+	return *this < BigInteger(num);
+}
+bool BigInteger::operator>=(unsigned int num)
+{
+	return *this >= BigInteger(num);
+}
+bool BigInteger::operator<=(unsigned int num)
+{
+	return *this <= BigInteger(num);
+}
+BigInteger& BigInteger::operator+=(BigInteger & num)
+{
+	*this = *this + num;
+	return *this;
+}
+BigInteger& BigInteger::operator+=(unsigned int  num)
+{
+	*this = *this + BigInteger(num);
+	return *this;
+}
+BigInteger& BigInteger::operator*=(BigInteger & num)
+{
+	*this = *this * num;
+	return *this;
+}
+BigInteger& BigInteger::operator*=(unsigned int  num)
+{
+	*this = *this * BigInteger(num);
+	return *this;
+}
+int BigInteger::toInt()
+{
+	int num = 0;
+	string pnum = this->toString();
+	for (int i = 0; i < pnum.length(); i++)
+	{
+		num *= 10;
+		num += pnum[i] - '0';
+	}
+	return num;
+}
+double BigInteger::toDouble()
+{
+	double num = 0;
+	string pnum = this->toString();
+	for (int i = 0; i < pnum.length(); i++)
+	{
+		num *= 10;
+		num += pnum[i] - '0';
+	}
+	return num;
+}
+BigInteger BigInteger::operator << (BigInteger & num)
+{
+	BigInteger ret = *this;
+	for (BigInteger i = 0; i < num; i++)
+	{
+		ret.m_num.push_front('0');
+	}
+	return ret;
+}
+BigInteger BigInteger::operator<<(unsigned int num)
+{
+	BigInteger ret = *this;
+	for (int i = 0; i < num; i++)
+	{
+		ret.m_num.push_front('0');
+	}
+	return ret;
+}
+
+BigInteger BigInteger::operator>>(BigInteger & num)
+{
+	BigInteger ret = *this;
+	for (BigInteger i = 0; i < num; i++)
+	{
+		if (ret.m_num.empty())
+		{
+			ret.m_num.push_front('0');
+			break;
+		}
+		ret.m_num.pop_front();
+	}
+	return ret;
+}
+BigInteger BigInteger::operator>>(unsigned int num)
+{
+	BigInteger ret = *this;
+	for (int i = 0; i < num; i++)
+	{
+		if (ret.m_num.empty())
+		{
+			ret.m_num.push_front('0');
+			break;
+		}
+		ret.m_num.pop_front();
+	}
+	return ret;
+}
+BigInteger BigInteger::operator~()
+{
+	BigInteger ret = *this;
+	for (deque<char>::iterator i = ret.m_num.begin(); i != ret.m_num.end(); i++)
+	{
+		*i = (9 - (*i - '0')) + '0';
+	}
+	return ret;
+}
+BigInteger  BigInteger::operator-(BigInteger & num)
+{
+	return BigInteger(Sub(num));
+}
+string BigInteger::Sub(BigInteger & num)
+{
+	string pthis = this->toString();
+	string pnum = num.toString();
+	BigInteger mthis = *this;
+	BigInteger mnum = num;
+	if (pthis.length() > pnum.length())
+	{
+		for (int i = 0; i < pthis.length() - pnum.length(); i++)
+		{
+			mnum.m_num.push_back('0');
+		}
+	}
+	else if (pthis.length() < pnum.length())
+	{
+		for (int i = 0; i < pnum.length() - pthis.length(); i++)
+		{
+			mthis.m_num.push_back('0');
+		}
+	}
+	mthis.m_num.push_back('0');
+	mnum = ~mnum;
+	mnum.m_num.push_back('1');
+	mnum++;
+	BigInteger sum = mthis + mnum;
+	int maxlen = pthis.length() > pnum.length() ? pthis.length() : pnum.length();
+	char dona = '+';
+	if ((sum.m_num[maxlen] - '0') % 2 == 0)
+		dona = sum.m_num[maxlen] = '+';
+	else
+	{
+		sum = ~sum + 1;
+		dona = sum.m_num[maxlen] = '-';
+	}
+	while (true)
+	{
+		if (sum.m_num.empty())
+		{
+			sum.m_num.push_back('0');
+			break;
+		}
+		if (sum.m_num.back() == '+' || sum.m_num.back() == '-')
+		{
+			sum.m_num.pop_back();
+		}
+		if (sum.m_num.back() != '0')
+		{
+			break;
+		}
+		else
+		{
+			sum.m_num.pop_back();
+		}
+
+	}
+	sum.m_num.push_back(dona);
+	return sum.toString();
+}
+BigInteger BigInteger::operator-(unsigned int num)
+{
+	return *this - BigInteger(num);
+}
+BigInteger& BigInteger::operator-=(BigInteger & num)
+{
+	*this=*this - num;
+	return *this;
+}
+BigInteger& BigInteger::operator-=(unsigned int num)
+{
+	*this = *this - num;
+	return *this;
+}
+BigInteger& BigInteger::operator--()
+{
+	*this = *this - BigInteger(1);
+	return *this;
+}
+BigInteger BigInteger::operator--(int)
+{
+	BigInteger ret = *this;
+	*this = *this - BigInteger(1);
+	return ret;
 }
--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -29,19 +29,63 @@ void BigIntMul()
 	BigInteger bnum ( snum2);
 	cout << "result: " << (anum*bnum) << endl;
 }
+void PowXN()
+{
+	string snumx, snumn;
+	cout << "Please a pow x:>/ ";
+	cin >> snumx;
+	cout << "Please a pow n:>/ ";
+	cin >> snumn;
+	BigInteger anum(snumx);
+	BigInteger bnum(snumn);
+	cout << "result: " << anum.pow(bnum) << endl;
+}
+void FactorialN()
+{
+	string snum;
+	cout << "Please a Factorial number:>/ ";
+	cin >> snum;
+	BigInteger anum(snum);
+	cout << "result: " << anum.factorial() << endl;
+}
+void BigIntSub()
+{
+	string snum1, snum2;
+	cout << "Please a sub number:>/ ";
+	cin >> snum1;
+	cout << "Please a sub number:>/ ";
+	cin >> snum2;
+	BigInteger anum(snum1);
+	BigInteger bnum(snum2);
+	cout << "result: " << anum.Sub(bnum) << endl;
+}
 int main()
 {
 	system("Color f1");
 	system("title Fibonacci Sqe Big Integer dev Ice2Faith");
+	//BigInteger fp("52");
+	//BigInteger sp(47);
+	//cout << fp - sp << endl;
+	//cout << sp - fp << endl;
+	//cout << fp.Sub( sp) << endl;
+	//cout << sp.Sub( fp) << endl;
+	//cout << fp-- << endl;
+	//cout << --fp << endl;
+	//cout << sp - 5 << endl;
+	//sp -= BigInteger(10);
+	//cout << sp << endl;
 	bool flag = true;
 	while (flag)
 	{
 		cout << "1.Fibonacci sqe" << endl;
 		cout << "2.Big Integer mul" << endl;
-		cout << "3.clean screen" << endl;
+		cout << "3.Pow x ^ n" << endl;
+		cout << "4.Factorial X!" << endl;
+		cout << "5.Big Integer Sub" << endl;
+		cout << "6.clean screen" << endl;
 		cout << "0.exit" << endl;
 		char sel = 0;
-		while (sel<'0' || sel>'3')
+		while (sel<'0' || sel>'6')
 			sel = getch();
 		switch (sel - '0')
 		{
@@ -55,6 +99,15 @@ int main()
 			BigIntMul();
 			break;
 		case 3:
+			PowXN();
+			break;
+		case 4:
+			FactorialN();
+			break;
+		case 5:
+			BigIntSub();
+			break;
+		case 6:
 			system("cls");
 			break;
 		}
